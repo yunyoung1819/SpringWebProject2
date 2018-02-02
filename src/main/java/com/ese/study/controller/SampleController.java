d72529acec8a75b94716500a0f85ffd23b72854e
Yoon Young <yunyoung1819@hanmail.net>
2018-02-02 23:53:37 +0900

    ajax 댓글처리

--- src/main/java/com/ese/study/controller/SampleController.java
+++ src/main/java/com/ese/study/controller/SampleController.java
@@ -1,8 +1,12 @@
 package com.ese.study.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -53,4 +57,44 @@ public class SampleController{
 		
 		return list;
 	}
-}
+	
+	// 컬렉션 타입의 객체를 반환하는 경우 (Map)
+	@RequestMapping("/sendMap")
+	public Map<Integer, SampleVO> sendMap(){
+		
+		Map<Integer, SampleVO> map =  new HashMap();
+		
+		for(int i=0; i<10; i++){
+			SampleVO vo = new SampleVO();
+			vo.setFirstName("수빈");
+			vo.setLastName("장");
+			vo.setMno(i);
+			map.put(i, vo);
+		}
+		
+		return map;
+	}
+	
+	// ResponseEntity 타입 (결과 데이터 + HTTP 상태 코드)
+	@RequestMapping("/sendErrorAuth")
+	public ResponseEntity<Void> sendListAuth(){
+		
+		return new ResponseEntity(HttpStatus.BAD_REQUEST);
+	}
+	
+	// ResponseEntity 타입 (결과 데이터 + HTTP 상태 코드)
+	@RequestMapping("/sendErrorNot")
+	public ResponseEntity<List<SampleVO>> sendListNot(){
+		
+		List<SampleVO> list = new ArrayList<SampleVO>();
+		for(int i=0; i<10; i++){
+			SampleVO vo = new SampleVO();
+			vo.setFirstName("길동");
+			vo.setLastName("홍");
+			vo.setMno(i);
+			list.add(vo);
+		}
+		
+		return new ResponseEntity(list, HttpStatus.NOT_FOUND);
+	}
+}                                                  

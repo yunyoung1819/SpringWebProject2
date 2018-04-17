eb196253e0a8309172398af25ea279591632e8dc
Yoon Young <yunyoung1819@hanmail.net>
2018-04-17 23:47:41 +0900

    interceptor preHandle Object handler

--- src/main/java/com/ese/interceptor/SampleInterceptor.java
+++ src/main/java/com/ese/interceptor/SampleInterceptor.java
@@ -1,10 +1,15 @@
 package com.ese.interceptor;
 
+import java.lang.reflect.Method;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.sun.net.ssl.internal.www.protocol.https.Handler;
 
 /**
  * 로그인 처리
@@ -27,6 +32,11 @@ public class SampleInterceptor extends HandlerInterceptorAdapter {
 		
 		System.out.println("pre handle...........");
 		
+		HandlerMethod method = (HandlerMethod) handler;
+		Method methodObj = method.getMethod();
+		
+		System.out.println("Bean : " + method.getBean());
+		System.out.println("Method : " + methodObj);
 		return true;
 	}
 
